82e67d66dd625a6ff93b8d28c7a050f191ea98cd
katya0112 <hdzkatya07@gmail.com>
2022-09-05 08:54:13 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/presentaciondemifamilia/PresentacionDeMiFamilia.java
@@ -0,0 +1,26 @@
+
+package com.mycompany.presentaciondemifamilia;
+
+/**
+ *
+ * @author shoto
+ */
+public class PresentacionDeMiFamilia {
+
+    public static void main(String[] args) {
+    int familia=5;
+        
+        System.out.println("Esta es mi familia");
+        System.out.println("mi mamá es: Yaneth García");
+        System.out.println("su edad es: 37 ");
+        System.out.println("mi papá es: Jorge Hernández");
+        System.out.println("su edad es: 39");
+        System.out.println("mi hermano es: Stiven Hernández");
+        System.out.println("su edad es: 16 ");
+        System.out.println("mi hermana es: Judith Hernández");
+        System.out.println("su edad es: 20 ");
+        System.out.println("mi nombre es: Katya Hernández");
+        System.out.println("mi edad es: 18 ");
+        
+    }
+}
